e4f2e1094bb55e2643f6224ea6ff005e7ca95d2d
Chathurya Prasad <121708803+CPrasa@users.noreply.github.com>
2024-03-17 18:15:40 +0530

    lambda functions

---
+++ 7_Lambda.py
@@ -0,0 +1,7 @@
+
+x = lambda a,b: a+b
+print(x(5,6))
+
+
+y = lambda l,m,n : l*m/n
+print(y(4,3,2))
--- if else.py
+++ if else.py
@@ -2,3 +2,16 @@ if 5<2:
   print("5 id greter than two")
 else:
   print("2 is less than 5")
+  
+  
+  
+x = 150
+y = 5
+
+if x > 15 & y < 10 :
+  print("Hello World")
+  
+else :
+  print("worng answer")
+  
+  
