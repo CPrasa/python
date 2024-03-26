8427c2153f4b53f5ac8f27a0afa771a0e62edda3
Chathurya Prasad <121708803+CPrasa@users.noreply.github.com>
2024-03-26 19:23:48 +0530

    print without vowels

---
+++ 10_for_loop.py
@@ -0,0 +1,13 @@
+
+### print only vowels
+
+list = ['a', 'b', 'd', 'f','o','f','l','k','e','u']
+list_a = ['a','e','i','o','u']
+for i in list:
+  if i in list_a:
+    print(i) 
+  
+  
+###print name vowels
+
+  
--- WhileLop.py
+++ WhileLop.py
@@ -1,7 +1,7 @@
 
 
 list = ['a', 'b', 'd', 'f','o', 'f','l','k','e','u']
-i = 0 
-while i < len(list):
-  listLength = len(list)
-  print(list[i])
+
+for i in list:
+  print(i)
+ 
