17fcfe3c2d1104ac5cbb794a8ab4e5fbfa825e7e
Chathurya Prasad <121708803+CPrasa@users.noreply.github.com>
2024-05-03 23:36:48 +0530

    add features to student

--- Inheritance/Person.py
+++ Inheritance/Person.py
@@ -15,7 +15,14 @@ x.printname()
 
 
 class Student(Person):
-  pass
+  def __init__(self, fname, lname, school, grade):
+    super().__init__(self, fname, lname)
+    self.school = school
+    self.grade = grade
+    
+
+  
+  
 
 
 y = Student("Chathurya", "Prasad")
