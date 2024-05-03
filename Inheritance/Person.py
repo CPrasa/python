379759672032143f31f38ba21f694849dc1baba0
Chathurya Prasad <121708803+CPrasa@users.noreply.github.com>
2024-05-03 23:42:11 +0530

    save all

--- Inheritance/Person.py
+++ Inheritance/Person.py
@@ -1,29 +1,26 @@
 class Person:
-  def __init__(self, fname, lname):
-    self.firstname = fname
-    self.lastname = lname
-    
-  def printname(self):
-    print(self.firstname, self.lastname)
-    
-    
-  #Use the person class to create an object, and then execute the printname method:
-  
-x = Person("John","Doe")
-x.printname()
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
 
+    def printname(self):
+        print(self.firstname, self.lastname)
 
 
 class Student(Person):
-  def __init__(self, fname, lname, school, grade):
-    super().__init__(self, fname, lname)
-    self.school = school
-    self.grade = grade
-    
+    def __init__(self, fname, lname, school, grade):
+        super().__init__(fname, lname)
+        self.school = school
+        self.grade = grade
 
-  
-  
+    def print_details(self):
+        print("Name:", self.firstname, self.lastname)
+        print("School:", self.school)
+        print("Grade:", self.grade)
 
 
-y = Student("Chathurya", "Prasad")
-y.printname()
+x = Person("John", "Doe")
+x.printname()
+
+y = Student("Chathurya", "Prasad", "XYZ School", 10)
+y.print_details()
