3161376ab0587b8b7183e73cca27e4866fc186f3
Chathurya Prasad <121708803+CPrasa@users.noreply.github.com>
2024-05-05 23:43:32 +0530

    add class

--- Encapsulation/encapsulation.py
+++ Encapsulation/encapsulation.py
@@ -7,7 +7,20 @@ class Base:
 		# Protected member 
 		self._a = 2
 
+# Creating a derived class 
+class Derived(Base): 
+	def __init__(self): 
 
+		# Calling constructor of 
+		# Base class 
+		Base.__init__(self) 
+		print("Calling protected member of base class: ", 
+			self._a) 
+
+		# Modify the protected variable: 
+		self._a = 3
+		print("Calling modified protected member outside class: ", 
+			self._a) 
 
 
 obj1 = Derived() 
