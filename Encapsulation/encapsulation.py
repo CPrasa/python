fc83e40325a5f983c6d1413f4ce3151069d4678e
Chathurya Prasad <121708803+CPrasa@users.noreply.github.com>
2024-05-06 23:11:32 +0530

    modified the protected variable

--- Encapsulation/encapsulation.py
+++ Encapsulation/encapsulation.py
@@ -15,4 +15,6 @@ class Derived(Base):
     Base.__init__(self)
     print("Calling protected member of base class: " self._a)
     
-    
+    #Modifiy the protected variable:
+    self._a =3
+    print("calling modified proteccted member outside class: ", self._a)
