904a9747d027c04c430db1cde4dd1b95f1f7b204
Chathurya Prasad <121708803+CPrasa@users.noreply.github.com>
2024-05-10 23:32:24 +0530

    setter done

--- Encapsulation/gettersSetters.py
+++ Encapsulation/gettersSetters.py
@@ -7,9 +7,15 @@ class Car():
     
   def get_color(self):
     return(self.__color)
+  
+  def set_color(self, x):
+    self.__color = x
     
 car1 = Car("Mastang","Blue")
 
 print(car1.name)
+print(car1.get_color())
+
+car1.set_color("Red")
 print(car1.get_color())
   
