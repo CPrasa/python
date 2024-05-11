3ac416de4ee7885bb77ce4b2228222557c81e349
Chathurya Prasad <121708803+CPrasa@users.noreply.github.com>
2024-05-12 00:14:29 +0530

    creted abstract class

--- Abstract/abstractclass.py
+++ Abstract/abstractclass.py
@@ -2,15 +2,12 @@
 #A class that contains one or more abstract methods is called an abstract class.
 #A abstract method that method has declaration but does not have implementation
 
+#we need to import module of abc
 
 
+from abc import ABC
 
-import abc 
+class Phone(ABC):
   
-  
-class AbstractClass(metaclass=abc.ABCMeta): 
-    def abstractfunc(self): 
-        return None
-  
-  
-print(AbstractClass.register(dict)) 
+  def func(self):
+    pass
