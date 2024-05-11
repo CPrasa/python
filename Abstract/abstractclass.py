59117750557c446a8a3960b060ab7d8cec7cd102
Chathurya Prasad <121708803+CPrasa@users.noreply.github.com>
2024-05-12 00:18:12 +0530

    create abstract class and inherite chiled class

--- Abstract/abstractclass.py
+++ Abstract/abstractclass.py
@@ -5,9 +5,18 @@
 #we need to import module of abc
 
 
-from abc import ABC
+from abc import ABC, abstractclassmethod
 
 class Phone(ABC):
-  
+  @abstractclassmethod
   def func(self):
     pass
+  
+  
+#obj1 = Phone()
+
+class Samsung(Phone):
+  def func(self):
+    pass
+
+obj2 = Samsung()
